b1b53a330c09c7ccf1491138fea9f3e12726df9a
daimatz <dai@daimatz.net>
2021-09-23 04:55:01 +0900

    add env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TITLE = 'React GraphQL GitHub Client';
+
+const axiosGithubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: 'bearer ' + process.env.GH_TOKEN,
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{TITLE}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
